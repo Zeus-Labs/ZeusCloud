177c1a138e73b358b344dc44421dd6eb0793bf44
Vishal Jain <vishaljain97@gmail.com>
2023-03-14 13:43:32 -0700

    Private VM Admin rule.

--- backend/rules/attackpath/PrivateVmAdmin.go
+++ backend/rules/attackpath/PrivateVmAdmin.go
@@ -6,46 +6,51 @@ import (
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
 
-type PrivateServerlessAdmin struct{}
+type PrivateVmAdmin struct{}
 
-func (PrivateServerlessAdmin) UID() string {
-	return "attackpath/private_serverless_admin_permissions"
+func (PrivateVmAdmin) UID() string {
+	return "attackpath/private_vm_admin_permissions"
 }
 
-func (PrivateServerlessAdmin) Description() string {
-	return "Private serverless function with effective admin permissions."
+func (PrivateVmAdmin) Description() string {
+	return "Private VM instance with effective admin permissions."
 }
 
-func (PrivateServerlessAdmin) Severity() types.Severity {
+func (PrivateVmAdmin) Severity() types.Severity {
 	return types.High
 }
 
-func (PrivateServerlessAdmin) RiskCategories() types.RiskCategoryList {
+func (PrivateVmAdmin) RiskCategories() types.RiskCategoryList {
 	return []types.RiskCategory{
 		types.IamMisconfiguration,
 	}
 }
 
-func (PrivateServerlessAdmin) Execute(tx neo4j.Transaction) ([]types.Result, error) {
+func (PrivateVmAdmin) Execute(tx neo4j.Transaction) ([]types.Result, error) {
 	records, err := tx.Run(
-		`MATCH (a:AWSAccount{inscope: true})-[:RESOURCE]->(lambda:AWSLambda)
+		`MATCH (a:AWSAccount{inscope: true})-[:RESOURCE]->(e:EC2Instance)
+		OPTIONAL MATCH
+			(:IpRange{id: '0.0.0.0/0'})-[:MEMBER_OF_IP_RULE]->
+			(:IpPermissionInbound)-[:MEMBER_OF_EC2_SECURITY_GROUP]->
+			(instance_group:EC2SecurityGroup)<-[:MEMBER_OF_EC2_SECURITY_GROUP|NETWORK_INTERFACE*..2]-(e)
+		WITH a, e, collect(distinct instance_group.id) as instance_group_ids
 		OPTIONAL MATCH
 			(:IpRange{range:'0.0.0.0/0'})-[:MEMBER_OF_IP_RULE]->
 			(perm:IpPermissionInbound)-[:MEMBER_OF_EC2_SECURITY_GROUP]->
 			(elbv2_group:EC2SecurityGroup)<-[:MEMBER_OF_EC2_SECURITY_GROUP]-
 			(elbv2:LoadBalancerV2{scheme: 'internet-facing'})—[:ELBV2_LISTENER]->
 			(listener:ELBV2Listener),
-			(elbv2)-[:EXPOSE]->(lambda)
+			(elbv2)-[:EXPOSE]->(e)
 		WHERE listener.port >= perm.fromport AND listener.port <= perm.toport
-		WITH a, lambda, collect(distinct elbv2.id) as public_elbv2_ids
+		WITH a, e, instance_group_ids, collect(distinct elbv2.id) as public_elbv2_ids
 		OPTIONAL MATCH
-			(lambda)-[:STS_ASSUME_ROLE_ALLOW]->(role:AWSRole{is_admin: True})
-		WITH a, lambda, public_elbv2_ids, collect(role.arn) as admin_roles, collect(role.admin_reason) as admin_reasons
-		WITH a, lambda, public_elbv2_ids, admin_roles, admin_reasons,
-		size(public_elbv2_ids) = 0 as private,
+			(e)-[:STS_ASSUME_ROLE_ALLOW]->(role:AWSRole{is_admin: True})
+		WITH a, e, instance_group_ids, public_elbv2_ids, collect(role.arn) as admin_roles, collect(role.admin_reason) as admin_reasons
+		WITH a, e, instance_group_ids, public_elbv2_ids, admin_roles, admin_reasons,
+		(e.publicipaddress IS NULL OR size(instance_group_ids) = 0) AND size(public_elbv2_ids) = 0 as private,
 		(size(admin_roles) > 0) as is_admin
-		RETURN lambda.id as resource_id,
-		'AWSLambda' as resource_type,
+		RETURN e.id as resource_id,
+		'EC2Instance' as resource_type,
 		a.id as account_id,
 		CASE 
 			WHEN private AND is_admin THEN 'failed'
@@ -53,9 +58,9 @@ func (PrivateServerlessAdmin) Execute(tx neo4j.Transaction) ([]types.Result, err
 		END as status,
 		CASE 
 			WHEN is_admin THEN (
-				'The function is effectively an admin in the account because of: ' + admin_reasons[0] + '.'
+				'The instance is effectively an admin in the account because of: ' + admin_reasons[0] + '.'
 			)
-			ELSE 'The function was not detected as effectively an admin in the account.'
+			ELSE 'The instance was not detected as effectively an admin in the account.'
 		END as context`,
 		nil,
 	)
