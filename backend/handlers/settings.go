4cbeb0830830dd737f639738299273b367c22e4b
vishjain <vishaljain97@gmail.com>
2023-04-21 10:46:24 -0700

    Changes to demo (#214)

--- backend/handlers/settings.go
+++ backend/handlers/settings.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -10,6 +12,11 @@ import (
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
+
+type AccountError struct {
+	Error     error
+	ErrorCode int
+}
 
 func GetAccountDetails(postgresDb *gorm.DB) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -35,23 +42,90 @@ func GetAccountDetails(postgresDb *gorm.DB) func(w http.ResponseWriter, r *http.
 	}
 }
 
+func ManualAddAccountDetails(postgresDb *gorm.DB, ad models.AccountDetails) AccountError {
+	// Only allow 1 account details row for now
+	var accountDetailsLst []models.AccountDetails
+	if tx := postgresDb.Find(&accountDetailsLst); tx.Error != nil {
+		log.Printf("failed to retrieve account details: %v", tx.Error)
+		return AccountError{
+			Error:     errors.New("failed to retrieve account details"),
+			ErrorCode: 400,
+		}
+	}
+	if len(accountDetailsLst) > 0 {
+		log.Printf("Sorry! ZeusCloud only supports 1 account currently.")
+		return AccountError{
+			Error:     errors.New("Sorry! ZeusCloud only supports 1 account currently."),
+			ErrorCode: 400,
+		}
+	}
+
+	if ad.ConnectionMethod != "profile" && ad.ConnectionMethod != "access_key" {
+		log.Printf("Invalid connection method: %v", ad.ConnectionMethod)
+		return AccountError{
+			Error:     errors.New("Invalid connection method. Must be profile or access_key"),
+			ErrorCode: 400,
+		}
+	}
+	if ad.ConnectionMethod == "profile" {
+		apr, err := control.GetAwsProfiles()
+		if err != nil {
+			log.Printf("Failed to find available aws profiles: %v", err)
+			return AccountError{
+				Error:     errors.New("Failed to find available aws profiles"),
+				ErrorCode: 500,
+			}
+		}
+		var found bool
+		for _, profile := range apr.AwsProfiles {
+			if profile == ad.Profile {
+				found = true
+				break
+			}
+		}
+		if !found {
+			log.Printf("Invalid profile: %v. Expected one of %v", ad.Profile, apr.AwsProfiles)
+			return AccountError{
+				Error:     errors.New("invalid profile"),
+				ErrorCode: 400,
+			}
+		}
+		ad.AwsAccessKeyId = ""
+		ad.AwsSecretAccessKey = ""
+		ad.DefaultRegion = ""
+	} else {
+		ad.Profile = ""
+	}
+	ad.LastScanCompleted = nil
+
+	// Attempt to insert into database
+	tx := postgresDb.Clauses(clause.OnConflict{DoNothing: true}).Create(&ad)
+	if tx.Error != nil {
+		log.Printf("failed to add account details: %v", tx.Error)
+		return AccountError{
+			Error:     errors.New("failed to add account details"),
+			ErrorCode: 400,
+		}
+	}
+	if tx.RowsAffected != 1 {
+		errorStr := fmt.Sprintf("expected 1 row to be affected not %v", tx.RowsAffected)
+		log.Printf(errorStr)
+		return AccountError{
+			Error:     errors.New("duplicate account"),
+			ErrorCode: 400,
+		}
+	}
+
+	return AccountError{
+		Error:     nil,
+		ErrorCode: 200,
+	}
+}
+
 func AddAccountDetails(postgresDb *gorm.DB) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		// Only allow 1 account details row for now
-		var accountDetailsLst []models.AccountDetails
-		if tx := postgresDb.Find(&accountDetailsLst); tx.Error != nil {
-			log.Printf("failed to retrieve account details: %v", tx.Error)
-			http.Error(w, "failed get account details", 500)
-			return
-		}
-		if len(accountDetailsLst) > 0 {
-			log.Printf("Sorry! ZeusCloud only supports 1 account currently.")
-			http.Error(w, "Sorry! ZeusCloud only supports 1 account currently.", 400)
 			return
 		}
 
@@ -62,48 +136,11 @@ func AddAccountDetails(postgresDb *gorm.DB) func(w http.ResponseWriter, r *http.
 			http.Error(w, "failed to decode json body", 400)
 			return
 		}
-		if ad.ConnectionMethod != "profile" && ad.ConnectionMethod != "access_key" {
-			log.Printf("Invalid connection method: %v", ad.ConnectionMethod)
-			http.Error(w, "Invalid connection method. Must be profile or access_key", 400)
-			return
-		}
-		if ad.ConnectionMethod == "profile" {
-			apr, err := control.GetAwsProfiles()
-			if err != nil {
-				log.Printf("Failed to find available aws profiles: %v", err)
-				http.Error(w, "Failed to find available aws profiles", 500)
-				return
-			}
-			var found bool
-			for _, profile := range apr.AwsProfiles {
-				if profile == ad.Profile {
-					found = true
-					break
-				}
-			}
-			if !found {
-				log.Printf("Invalid profile: %v. Expected one of %v", ad.Profile, apr.AwsProfiles)
-				http.Error(w, "Invalid profile", 400)
-				return
-			}
-			ad.AwsAccessKeyId = ""
-			ad.AwsSecretAccessKey = ""
-			ad.DefaultRegion = ""
-		} else {
-			ad.Profile = ""
-		}
-		ad.LastScanCompleted = nil
 
-		// Attempt to insert into database
-		tx := postgresDb.Clauses(clause.OnConflict{DoNothing: true}).Create(&ad)
-		if tx.Error != nil {
-			log.Printf("failed to add account details: %v", tx.Error)
-			http.Error(w, "failed to add account details", 400)
-			return
-		}
-		if tx.RowsAffected != 1 {
-			log.Printf("expected 1 row to be affected not %v", tx.RowsAffected)
-			http.Error(w, "duplicate account", 400)
+		accountError := ManualAddAccountDetails(postgresDb, ad)
+		if accountError.ErrorCode != 200 || accountError.Error != nil {
+			log.Printf("Adding account error %+v", accountError)
+			http.Error(w, accountError.Error.Error(), accountError.ErrorCode)
 			return
 		}
 
