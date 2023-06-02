cc07f187a6f8ba3f98d8a493404f368305685d21
Tushar Goel <tushargoel.tg@gmail.com>
2023-06-02 12:49:09 +0530

    Imdsv1 attackpath completed (#295)
    * added the imdsv1 attackpath
    
    * cartography point to master
    
    * added the instructions for requiring imdsv2
    
    ---------
    
    Co-authored-by: Tushar Goel <tushargoel@MacBook-Air.local>

---
+++ frontend/src/Components/Remediation/AttackPathComponents/PubliclyExposedVmAdminIMDSv1Enabled.tsx
@@ -0,0 +1,34 @@
+import { NormalInstruction } from "../Instructions"
+import { StepInstruction } from "../Instructions"
+
+export const PubliclyExposedVmAdminIMDSv1Enabled = () => {
+    return <>
+    <NormalInstruction instruction="Perform the following steps."/>
+
+    <StepInstruction stepNumber={"1."} instruction="Check the administrative role 
+    associated with your publicly exposed VM."/>
+
+    <StepInstruction stepNumber={"2."} instruction="For the role, reduce the scope of the
+    permissions in the associated policies."/>
+
+    <StepInstruction stepNumber={"3."} instruction="Disable IMDSv1 by requiring IMDSv2 credentials for the publicly exposed VM."/>
+
+    <StepInstruction stepNumber={"4."} instruction="Open the Amazon EC2 console at https://console.aws.amazon.com/ec2/."/>
+
+    <StepInstruction stepNumber={"5."} instruction="In the navigation pane, choose Instances."/>
+
+    <StepInstruction stepNumber={"6."} instruction="Select your instance."/>
+
+    <StepInstruction stepNumber={"7."} instruction="Choose Actions, Instance settings, Modify instance metadata options."/>
+
+    <StepInstruction stepNumber={"8."} instruction="In the Modify instance metadata options dialog box, do the following:"/>
+
+    <StepInstruction stepNumber={"a."} subInstruction={true} instruction="For Instance metadata service, select Enable."/>
+
+    <StepInstruction stepNumber={"b."} subInstruction={true} instruction="For IMDSv2, choose Required."/>
+
+    <StepInstruction stepNumber={"c."} subInstruction={true} instruction="Choose Save."/>
+
+</>
+
+}
